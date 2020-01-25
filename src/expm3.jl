61d00cdf4abf7a4f5b34dbda9aa82130e38c100c
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2020-01-25 15:43:52 +0100

    Update expm3.jl

--- src/expm3.jl
+++ src/expm3.jl
@@ -1,6 +1,6 @@
 using LinearAlgebra
 import ExpmV.normAm
-
+import LinearAlgebra:BlasFloat, checksquare
 
 export expm3
 
@@ -16,124 +16,153 @@ to reduce overscaling
 
 The choice of order and scaling is based solely on norm(A,1)
 """
-function expm3(A)
+function expm3(A::StridedMatrix{T}) where {T<:BlasFloat}
 
-    s, degree, Tpowers = expm_params(A)
+    """
+        expm_params(A)
+    
+    expm_helper Obtain scaling parameter and order of the Taylor 
+    approximant
+    """
+    function expm_params(A)
+    
+        function getThetaSet(A)
+        
+            m_vals = [1 2 4 8 12 18]
+            if T == Float64
+        
+                # theta_m for m=2:18.
+                theta = [
+                    2.220446049250313e-16  # m_vals = 1
+                    2.580956802971767e-08  # m_vals = 2
+                    3.397168839976962e-04  # m_vals = 4
+                    4.991228871115323e-02  # m_vals = 8
+                    2.996158913811580e-01  # m_vals = 12
+                    1.090863719290036e+00
+                ]# m_vals = 18
+        
+            elseif T == Float32
+        
+                # theta_m for m=1:7.
+                theta = [
+                    1.192092800768788e-7   # m_vals = 1
+                    5.978858893805233e-04  # m_vals = 2 
+                    5.116619363445086e-02  # m_vals = 4
+                    5.800524627688768e-01  # m_vals = 8
+                   #7.795113374358031e-01
+                   #9.951840790004457e-01
+                   #1.223479542424143e+00
+                    1.461661507209034e+00  # m_vals = 12
+                    3.010066362817634e+00
+                ]# m_vals = 18
+        
+            else
+                throw("Input matrix element type must be Float32 or Float64")
+            end
+        
+            return theta
+        
+        end
+    
+    
+        s = 0
+    
+        theta = getThetaSet(A)
+        normA = opnorm(A, 1)
+    
+        Tp = []
+        scale = Int[]
+    
+        # Find appropriate order:
+        push!(Tp, copy(A))
+        push!(scale, 1)
+        ## Check if degree 1 is sufficient (No info available)
+        if (normA < theta[1])
+            degree = 1
+            return s, degree, Tp, scale
+        end
+        push!(Tp, A * A)
+        push!(scale, 2)
+        if (normA < theta[2])
+            degree = 2
+            return s, degree, Tp, scale
+        end
+        if (normA < theta[3])
+            degree = 4
+            return s, degree, Tp, scale
+        end
+        # At degree 8, we could do the first estimates
+        # with pairs (2,3), (3,4), (3,5), (2,9) [Also (2,5), (2,7)]
+    
+        if (normA < theta[4])
+            degree = 8
+            return s, degree, Tp, scale
+        end
+        push!(Tp, A * Tp[2])
+        push!(scale, 3)
+        if (normA < theta[5])
+            degree = 12
+            return s, degree, Tp, scale
+        end
+    
+        # Otherwise degree 18
+        # Here, we only gain the pairs (3, 8) and (4, 7), 
+        # but both are not expected to
+        # yield improvements, however, we also get (2, 2k+1) until (2,17)
+        degree = 18
+        push!(Tp, Tp[3] * Tp[3])
+        push!(scale, 6)
+        d2 = norm(Tp[2], 1)^(1 / 2)
+        d3 = norm(Tp[3], 1)^(1 / 3)
+        d6 = norm(Tp[4], 1)^(1 / 6)
+        eta = min(max(d2, d3), normA)
+        quotients = minimum([d2 / normA, d3 / normA, d6 / normA])
+        if (quotients < 1 / 2^4)  ## strong decay
+            d9 = normAm(A, 9)^(1 / 9)
+            d19 = normA
+            eta = min([eta, max(d2, d9), max(d2, d19)])
+        end
+        s = max(ceil(log2(eta / theta[6])), 0)
+        if isinf(s)
+            # Overflow in ell subfunction. Revert to old estimate.
+            x = opnorm(T, 1) / theta[end]
+            t, s = significand(x) / 2, exponent(x) + 1
+            s = s - Int(t == 0.5) # adjust s if normA/theta(end) is a power of 2.
+        end
+    
+        return s, degree, Tp, scale
 
-    @show s
+    end
+
+    s, degree, Tp, scale = expm_params(A)
+
     # Rescale the powers of A appropriately.
 
     if s != 0
-        for (k, v) in Tpowers
-            @show k
-            Tpowers[k] ./= 2 .^ (s * k)
+        for i in eachindex(Tp)
+            Tp[i] ./= 2 .^ (s * scale[i])
         end
     end
 
-    # Evaluate the Taylor polynomial to given degree.
-    E  = zero(A)
-    E .= taylor_approximant_of_degree(degree, Tpowers)
+    """
+        taylor_approximant_of_degree(order, Tp)
+    
+    Evaluate the Taylor polynomial to given degree.
+    Improved Paterson Stockmeyer scheme, with Pade at end point
+    """
 
-    for i = 1:s
-        E .= E * E  # Squaring
-    end
+    order = degree
 
-    E
-
-end
-
-"""
-    expm_params(A)
-
-expm_helper Obtain scaling parameter and order of the Taylor 
-approximant
-"""
-function expm_params(A)
-
-    s :: Int = 0
-
-    theta = getThetaSet(A)
-    @show normA = opnorm(A, 1)
-
-    Tp = Dict()
-
-    # Find appropriate order:
-    Tp[1] = A
-    ## Check if degree 1 is sufficient (No info available)
-    if (normA < theta[1])
-        degree = 1
-        return s, degree, Tp
-    end
-    Tp[2] = A * A
-    if (normA < theta[2])
-        degree = 2
-        return s, degree, Tp
-    end
-    if (normA < theta[3])
-        degree = 4
-        return s, degree, Tp
-    end
-    # At degree 8, we could do the first estimates
-    # with pairs (2,3), (3,4), (3,5), (2,9) [Also (2,5), (2,7)]
-
-    if (normA < theta[4])
-        degree = 8
-        return s, degree, Tp
-    end
-    Tp[3] = A * Tp[2]
-    if (normA < theta[5])
-        degree = 12
-        return s, degree, Tp
-    end
-
-    # Otherwise degree 18
-    # Here, we only gain the pairs (3, 8) and (4, 7), 
-    # but both are not expected to
-    # yield improvements, however, we also get (2, 2k+1) until (2,17)
-    degree = 18
-    Tp[6] = Tp[3] * Tp[3]
-    d2 = norm(Tp[2], 1)^(1 / 2)
-    d3 = norm(Tp[3], 1)^(1 / 3)
-    d6 = norm(Tp[6], 1)^(1 / 6)
-    eta = min(max(d2, d3), normA)
-    quotients = minimum([d2 / normA, d3 / normA, d6 / normA])
-    if (quotients < 1 / 2^4)  ## strong decay
-        d9 = normAm(A, 9)^(1 / 9)
-        d19 = normA
-        eta = min([eta, max(d2, d9), max(d2, d19)])
-    end
-    s = max(ceil(log2(eta / theta[6])), 0)
-    if isinf(s)
-        # Overflow in ell subfunction. Revert to old estimate.
-        x = opnorm(T, 1) / theta[end]
-        t, s = significand(x) / 2, exponent(x) + 1
-        s = s - Int(t == 0.5) # adjust s if normA/theta(end) is a power of 2.
-    end
-
-    return s, degree, Tp
-end
-
-"""
-    taylor_approximant_of_degree(order, Tp)
-
-Improved Paterson Stockmeyer scheme, with Pade at end point
-"""
-function taylor_approximant_of_degree(order, Tp)
-
-    A = Tp[1]
-    if order >= 4
-        A2 = Tp[2]
-    end
-    if order > 8
-        A3 = Tp[3]
-    end
-
+    X = Tp[1]
+    order >= 4 && (A2 = Tp[2])
+    order > 8  && (A3 = Tp[3])
+    
     if order == 1
-        E = I + A
+        E = I + X
     elseif order == 2
-        E = I + A
+        E = I + X
     elseif order == 4
-        E = I + A + A2 / 2 * (I + A / 3 + A2 / 3 / 4)
+        E = I + X + A2 / 2 * (I + X / 3 + A2 / 3 / 4)
     elseif order == 8
         # Minimizes ||coefficients||_1
         x3 = 2 / 3
@@ -147,9 +176,9 @@ function taylor_approximant_of_degree(order, Tp)
         c2 = (11 * (-9 + sqrt(177))) / (5040 * x3)
         c4 = -((-89 + sqrt(177)) / (5040 * x3^2))
         # Matrix products
-        A4 = A2 * (a1 * A + a2 * A2)
-        A8 = (x3 * A2 + 1 * A4) * (c0 * I + c1 * A + c2 * A2 + c4 * A4)
-        E = u0 * I + u1 * A + u2 * A2 + +A8
+        A4 = A2 * (a1 * X + a2 * A2)
+        A8 = (x3 * A2 + 1 * A4) * (c0 * I + c1 * X + c2 * A2 + c4 * A4)
+        E = u0 * I + u1 * X + u2 * A2 + +A8
     elseif order == 12 # lower values of ||_||_1
         a01 = -0.0186023205146205532243437300433
         a02 = 4.60000000000000000000000000000
@@ -167,10 +196,10 @@ function taylor_approximant_of_degree(order, Tp)
         a32 = 0.00172990000000000000000000000000
         a33 = 0.0107862779315792425026320640108
         a34 = -0.00675951846863086359778560766482
-        q31 = a01 * I + a11 * A + a21 * A2 + a31 * A3
-        q32 = a02 * I + a12 * A + a22 * A2 + a32 * A3
-        q33 = a03 * I + a13 * A + a23 * A2 + a33 * A3
-        q34 = a04 * I + a14 * A + a24 * A2 + a34 * A3
+        q31 = a01 * I + a11 * X + a21 * A2 + a31 * A3
+        q32 = a02 * I + a12 * X + a22 * A2 + a32 * A3
+        q33 = a03 * I + a13 * X + a23 * A2 + a33 * A3
+        q34 = a04 * I + a14 * X + a24 * A2 + a34 * A3
         # Matrix products
         q61 = q33 + q34 * q34
         E = (q31 + (q32 + q61) * q61)
@@ -200,60 +229,28 @@ function taylor_approximant_of_degree(order, Tp)
         b24 = -0.0923364619367118592764570775143
         b34 = -0.0169364939002081717191385115723
         b64 = -0.0000140086798182036159794363205726
-
+    
         # Extra matrix product
-        A6 = Tp[6]
-        q31 = a01 * I + a11 * A + a21 * A2 + a31 * A3
-        q61 = (b01 * I + b11 * A + b21 * A2 + b31 * A3 + b61 * A6)
-        q62 = (b02 * I + b12 * A + b22 * A2 + b32 * A3 + b62 * A6)
-        q63 = (b03 * I + b13 * A + b23 * A2 + b33 * A3 + b63 * A6)
-        q64 = (b04 * I + b14 * A + b24 * A2 + b34 * A3 + b64 * A6)
+        A6 = Tp[4]
+        q31 = a01 * I + a11 * X + a21 * A2 + a31 * A3
+        q61 = (b01 * I + b11 * X + b21 * A2 + b31 * A3 + b61 * A6)
+        q62 = (b02 * I + b12 * X + b22 * A2 + b32 * A3 + b62 * A6)
+        q63 = (b03 * I + b13 * X + b23 * A2 + b33 * A3 + b63 * A6)
+        q64 = (b04 * I + b14 * X + b24 * A2 + b34 * A3 + b64 * A6)
         q91 = q31 * q64 + q63
         q18 = q61 + (q62 + q91) * q91
         E = q18
     else
         throw("expm3= degree:$order")
     end
+    
 
-    return E
-
-end
-
-function getThetaSet(A)
-
-    classA = eltype(A)
-    m_vals = [1 2 4 8 12 18]
-    if classA == Float64
-
-        # theta_m for m=2:18.
-        theta = [
-            2.220446049250313e-16  # m_vals = 1
-            2.580956802971767e-08  # m_vals = 2
-            3.397168839976962e-04  # m_vals = 4
-            4.991228871115323e-02  # m_vals = 8
-            2.996158913811580e-01  # m_vals = 12
-            1.090863719290036e+00
-        ]# m_vals = 18
-
-    elseif classA == Float32
-
-        # theta_m for m=1:7.
-        theta = [
-            1.192092800768788e-7   # m_vals = 1
-            5.978858893805233e-04  # m_vals = 2 
-            5.116619363445086e-02  # m_vals = 4
-            5.800524627688768e-01  # m_vals = 8
-           #7.795113374358031e-01
-           #9.951840790004457e-01
-           #1.223479542424143e+00
-            1.461661507209034e+00  # m_vals = 12
-            3.010066362817634e+00
-        ]# m_vals = 18
-
-    else
-        throw("Input matrix element type must be Float32 or Float64")
+    for i = 1:s
+        E = E * E  # Squaring
     end
 
-    return theta
+    E
 
 end
+
+
